4359bc045b5947b8c30a7134770d4120c2e51254
NOURDIN Milly <nourdin.milly.lgm@gmail.com>
2021-03-16 14:08:52 +0100

    Affichage d'exercice et identification élève

--- testexo.py
+++ testexo.py
@@ -23,17 +23,43 @@ def get_bdd(numero, niv):
     cursor.close()
     cnx.close()
 
+
+def get_bdd_list_exo(niv):
+    cnx = sqlite3.connect('DVmath_exercice.db')  #acces base de donnée
+    cursor = cnx.cursor()
+    request_val = "SELECT exercice FROM creat_exo WHERE niveau = ?"  #requete
+    data = (niv)
+    cursor.execute(request_val, data)
+    resultat = cursor.fetchall()  #afficher plusieurs donnée en tableau
+    print(resultat)  #affiche le resultat de la requete
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
     print("Acces BDD Réussi !")
+
+
+def enter_you_name():
+    answer = input("Nom ? > ")
+    answer1 = input("Prénom ? > ")
+    answer2 = input("Niveau ? > ")
+    result = answer2
+    if answer2 == "6" or "5" or "4" or "3":
+        print("Elève :", answer, answer1, result, "ème")
+
+    return result  #renvoie le niveau entrer par l'élève
 
 
 # Programme principal
 
-
 def main():
 
-    num1 = 4
-    num2 = 5
-    get_bdd(num1, num2)
+    niveau = enter_you_name()  #récupere le niveau entrer par l'élève
+    num1 = 1  #le numero de l'exercice
+    print("-----------------------------------------------")
+    get_bdd(num1, niveau)  #execution de la fonction qui va afficher un exercice choisi en fonction du niveau
+    print("-----------------------------------------------")
+    get_bdd_list_exo(niveau)  #execution de la fonction qui va afficher l'integralité des exercice concerné par le niveau
 
 
 if __name__ == '__main__':
