0f126d021c2bb8511eb55427317e5a6bac542081
CRISTOUD Mattéo <cristoud.matteo.lgm@gmail.com>
2021-03-26 10:53:55 +0100

    utilisation de subprocess à la place de os car obselète

--- testexo_synthese.py
+++ testexo_synthese.py
@@ -6,22 +6,23 @@ Création: admin, le 09/03/2021
 
 
 # Imports
-from espeakng import ESpeakNG
+#from espeakng import ESpeakNG
 import sqlite3  #importation du module sqlite3
 import os
+import subprocess
 
 
 # Fonctions
 
 
 def get_bdd(numero, niv):
-    cnx = sqlite3.connect('DVmath_exercice.db')  #acces base de donnée
+    cnx = sqlite3.connect('DVmath_exercice.db')  #accès base de donnée
     cursor = cnx.cursor()
-    request_val = "SELECT exercice FROM creat_exo WHERE numero = ? and niveau = ?"  #requete
+    request_val = "SELECT exercice FROM creat_exo WHERE numero = ? and niveau = ?"  #requête
     data = (numero, niv)
     cursor.execute(request_val, data)
-    resultat = cursor.fetchall()  #afficher plusieurs donnée en tableau
-    print(resultat)  #affiche le resultat de la requete
+    resultat = cursor.fetchall()  #afficher plusieurs données en tableau
+    print(resultat)  #affiche le résultat de la requête
     cnx.commit()
     cursor.close()
     cnx.close()
@@ -29,13 +30,13 @@ def get_bdd(numero, niv):
 
 
 def get_bdd_list_exo(niv):
-    cnx = sqlite3.connect('DVmath_exercice.db')  #acces base de donnée
+    cnx = sqlite3.connect('DVmath_exercice.db')  #accès base de données
     cursor = cnx.cursor()
-    request_val = "SELECT exercice FROM creat_exo WHERE niveau = ?"  #requete
+    request_val = "SELECT exercice FROM creat_exo WHERE niveau = ?"  #requête
     data = (niv)
     cursor.execute(request_val, data)
-    resultat = cursor.fetchall()  #afficher plusieurs donnée en tableau
-    print(resultat)  #affiche le resultat de la requete
+    resultat = cursor.fetchall()  #afficher plusieurs données en tableau
+    print(resultat)  #affiche le resultat de la requête
     cnx.commit()
     cursor.close()
     cnx.close()
@@ -51,27 +52,31 @@ def enter_you_name():
     if answer2 == "6" or "5" or "4" or "3":
         print("Elève :", answer, answer1, result, "ème")
 
-    return result  #renvoie le niveau entrer par l'élève
+    return result  #renvoie le niveau entré par l'élève
 
 
 # Programme principal
 
 def main():
-    esng = ESpeakNG(voice='fr')
+    #esng = ESpeakNG(voice='fr')
 
 
-    niveau = enter_you_name()  #récupere le niveau entrer par l'élève
+    niveau = enter_you_name()  #récupere le niveau entré par l'élève
     num1 = 1  #le numero de l'exercice
     print("-----------------------------------------------")
     exo = get_bdd(num1, niveau)  #execution de la fonction qui va afficher un exercice choisi en fonction du niveau
 
     exo = str(exo)
+    #print(exo)
     #commande = "espeak-ng -a 200 -v mb-fr1 -s 150 '" + exo + "' --stdout | aplay
-    os.system("espeak-ng -a 200 -v mb-fr1 -s 150 \"%s\" --stdout | aplay" %exo)
+    #os.system("espeak-ng -a 200 -v mb-fr1 -s 150 \"%s\" --stdout | aplay" %exo)
+    commande = "espeak-ng -a 200 -v mb-fr1 -s 150 " + "\"" + exo + "\"" + " --stdout | aplay"
+    print(commande)
+    code_retour = subprocess.run(commande, shell=True)
     #esng.say(exo)
 
     print("-----------------------------------------------")
-    get_bdd_list_exo(niveau)  #execution de la fonction qui va afficher l'integralité des exercice concerné par le niveau
+    get_bdd_list_exo(niveau)  #execution de la fonction qui va afficher l'integralité des exercices concernés par le niveau
 
 
 if __name__ == '__main__':
