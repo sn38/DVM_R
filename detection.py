7fbd252eac3de96420e65536910127323e2ddc42
CRISTOUD Mattéo <cristoud.matteo.lgm@gmail.com>
2021-03-30 14:09:51 +0200

    création dictionnaire via csv

--- detection.py
+++ detection.py
@@ -12,10 +12,14 @@ import csv
 
 # Programme principal
 def main():
-    f = open('caracteres.csv')
-    csvfile = csv.reader(open("caracteres.csv"))
-    caracteres = dict(csvfile)
-    print(caracteres["150"])
+    with open('caracteres2.csv', mode='r') as f:                #https://www.youtube.com/watch?v=Hu4I3GAAOmA
+        dialect = csv.Sniffer().sniff(f.readline())
+        f.seek(0)
+        caracteres = list(csv.DictReader(f, dialect=dialect))
+    resistances = [elt['resistance'] for elt in caracteres]
+
+    dict_complet = dict(zip(resistances, caracteres))
+    print(dict_complet['175'])
 
 
 if __name__ == '__main__':
