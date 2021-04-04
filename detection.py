8a091beca5db7a49a65ec3001e673df83038f3c4
Mattéo CRISTOUD <cristoud.matteo.lgm@gmail.com>
2021-04-04 22:13:07 +0200

    input resistance + check valeur valide

--- detection.py
+++ detection.py
@@ -6,22 +6,36 @@ Création: admin, le 02/03/2021
 
 
 # Imports
-#from resistances import detect
+
 import csv
 # Fonctions
 
 # Programme principal
 def main():
-    with open('caracteres2.csv', mode='r') as f:                #ouverture fichier csv en lecture         #https://www.youtube.com/watch?v=Hu4I3GAAOmA
-        dialect = csv.Sniffer().sniff(f.readline())             #objet recap infos fichier csv (détermination séparateur sur ligne 0)
-        f.seek(0)                                               #retour à première ligne
-        caracteres = list(csv.DictReader(f, dialect=dialect))   #importation données sous forme liste
-    resistances = [elt['resistance'] for elt in caracteres]     #liste des valeurs de résistances
+    with open('caracteres3.csv', mode='r') as f:                # ouverture fichier csv en lecture
+        dialect = csv.Sniffer().sniff(f.readline())             # objet recap infos fichier csv (détermination séparateur sur ligne 0)
+        f.seek(0)                                               # retour à première ligne
+        caracteres = list(csv.DictReader(f, dialect=dialect))   # importation données sous forme liste
+    resistances = [elt['resistance'] for elt in caracteres]     # liste des valeurs de résistances
 
-    dict_complet = dict(zip(resistances, caracteres))           #création du dictionnaire (association liste résistances à caractères
-    print(dict_complet['170']['caractere'])                     #affichage caractère à une valeur de résistance
+    dict_complet = dict(zip(resistances, caracteres))           # création du dictionnaire (association liste résistances à caractères)
+    print("Entrer résistance: ")
+    val_res = input()
+    if val_res in dict_complet:                                 # vérif si valeur présent dans dico
+        caract = dict_complet[val_res]['caractere']
+        print('Caractère correspondant à', val_res, 'ohm:', caract)  # affichage caractère à une valeur de résistance
+    else:
+        print('Erreur de résistance')
+
+    # print(dialect.delimiter)   # affiche le délimiteur
 
 
 if __name__ == '__main__':
     main()
 # Fin
+
+# https://www.youtube.com/watch?v=Hu4I3GAAOmA l15
+
+
+#     for i in dict_complet.items():
+#         print(i)
