265e081df7b88de2643acb9349aeaf8046f0494e
Mattéo CRISTOUD <cristoud.matteo.lgm@gmail.com>
2021-04-09 11:12:54 +0200

    try except ouverture fichier csv

--- detection_test.py
+++ detection_test.py
@@ -12,15 +12,22 @@ import csv
 
 # Programme principal
 def main():
-    with open('caracteres3.csv', mode='r') as f:                # ouverture fichier csv en lecture
+    try:    # essai d'ouverture fichier
+        f = open('caracteres3d.csv', mode='r')                # ouverture fichier csv en lecture
+    except FileNotFoundError:   # si fichier non trouvé
+        print("Fichier non trouvé") # affchage message
+        raise   # raise de l'erreur
+    else:   # si ouverture réussie
         dialect = csv.Sniffer().sniff(f.readline())             # objet recap infos fichier csv (détermination séparateur sur ligne 0)
         f.seek(0)                                               # retour à première ligne
         caracteres = list(csv.DictReader(f, dialect=dialect))   # importation données sous forme liste
-    resistances = [elt['resistance'] for elt in caracteres]     # liste des valeurs de résistances
+        resistances = [elt['resistance'] for elt in caracteres]     # liste des valeurs de résistances
+        dict_complet = dict(zip(resistances, caracteres))           # création du dictionnaire (association liste résistances à caractères)
+        f.close()
 
-    dict_complet = dict(zip(resistances, caracteres))           # création du dictionnaire (association liste résistances à caractères)
     print("Entrer résistance: ")
     val_res = input()
+
     if val_res in dict_complet:                                 # vérif si valeur présent dans dico
         caract = dict_complet[val_res]['caractere']
         print('Caractère correspondant à', val_res, 'ohm:', caract)  # affichage caractère à une valeur de résistance
