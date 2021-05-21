b3bf97aac8aac13d70fa9f96f7b897599a2aa3ae
CRISTOUD Mattéo <cristoud.matteo.lgm@gmail.com>
2021-05-21 11:35:04 +0200

    namedtuple

--- namedtuple.py
+++ namedtuple.py
@@ -8,16 +8,26 @@ Création: admin, le 19/05/2021
 # Imports
 import csv
 from collections import namedtuple
+import codecs
 
 # Fonctions
 # Programme principal
 def main():
-    Caracteres = namedtuple('Caracteres', ['sbas', 'shaut', 'caractere'])
-    csvReader = csv.reader(open("caracteres4.csv", "r"))
+    Row = namedtuple('Caractere', ('sbas', 'shaut', 'caractere'))
 
-    print("Contents of the CSV file:")
-    for caracteres in map(Caracteres._make, csvReader):
-        print(caracteres.caractere, caracteres.sbas, caracteres.shaut)
+    with codecs.open('caracteres4.csv', 'r', 'utf-8') as f:
+        r = csv.reader(f, delimiter=';')
+        next(r)  # Skip header
+        rows = [Row(*l) for l in r]
+        print("content of csv file:")
+        print(rows)
+
+    #Caracteres = namedtuple('Caracteres', ['sbas', 'shaut', 'caractere'])
+    #csvReader = csv.reader(open("caracteres4.csv", "r"))
+
+    #print("Contents of the CSV file:")
+    #for caracteres in map(Caracteres._make, csvReader):
+    #    print(caracteres.caractere, caracteres.sbas, caracteres.shaut)
 
 
 if __name__ == '__main__':
