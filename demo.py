fc64960420d4410729882040379c78cf7b0e5afb
CRISTOUD Mattéo <cristoud.matteo.lgm@gmail.com>
2021-06-04 13:46:21 +0200

    modif démo pour tests

--- demo.py
+++ demo.py
@@ -29,43 +29,53 @@ def build_dico(file):  # construction du dictionnaire à partir du CSV
     return dict_complet  # renvoi dictionnaire construit
 
 
-def synthese_vocale(message):  # fonction de lecture via synthèse vocale
-    commande = "espeak-ng -a 200 -v mb-fr1 -s 150 " + "\"" + message + "\"" + " --stdout | aplay"  # préparation de la commande
-    print(commande)  # debug: affichage de la commande avant exécution
-    resultat_exec = subprocess.run(commande,
-                                   shell=True)  # exécution de la commande dans shell et récup objet de subprocess
-    if resultat_exec.returncode != 0:  # vérif si returncode diff de 0
-        print("débug: Erreur dans la lecture")
-    else:
-        print("débug: Execution reussie")
+# def synthese_vocale(message):  # fonction de lecture via synthèse vocale
+#     commande = "espeak-ng -a 200 -v mb-fr1 -s 150 " + "\"" + message + "\"" + " --stdout | aplay"  # préparation de la commande
+#     print(commande)  # debug: affichage de la commande avant exécution
+#     resultat_exec = subprocess.run(commande,
+#                                    shell=True)  # exécution de la commande dans shell et récup objet de subprocess
+#     if resultat_exec.returncode != 0:  # vérif si returncode diff de 0
+#         print("débug: Erreur dans la lecture")
+#     else:
+#         print("débug: Execution reussie")
 
 
 def check_dico(dico, valres):  # lecture du dictionnaire pour reconnaissance caractère
     for key, value in dico.items():
-        seuil_bas = dico[key].get('sbas')
-        seuil_bas = int(seuil_bas)
-        seuil_haut = dico[key].get('shaut')
-        seuil_haut = int(seuil_haut)
-        if seuil_bas <= valres <= seuil_haut:
-            caractere = dico[key].get('caractere')
+        seuil_bas = dico[key].get('sbas')   # obtention valeur de sbas
+        seuil_bas = int(seuil_bas)  # conversion en int
+        seuil_haut = dico[key].get('shaut') # obtention valeur de shaut
+        seuil_haut = int(seuil_haut)    # conversion en int
+        if seuil_bas <= valres <= seuil_haut:   # si valeur donnée en paramètres comprise entre sbas et shaut
+            caractere = dico[key].get('caractere')  # on récupère le caractère correspondant
             #print("débug: ", caractere)  # debug
             return caractere
     try:
         caractere   # si variable caractere non créée
     except NameError:
-        synthese_vocale("Erreur de détection!")  # lecture erreur
+        print("erreur")
+        #synthese_vocale("Erreur de détection!")  # lecture erreur
         raise  # raise d'une erreur
 
 
 # Programme principal
 def main():
+    # val1 = 15
+    # val2 = 130
+    # val3 = 15
     dictionnaire = dico_csv()  # build du dictionnaire
-    caractere1 = check_dico(dictionnaire, 15)
-    print(caractere1)
-    caractere2 = check_dico(dictionnaire, 38)
-    print(caractere2)
-    caractere3 = check_dico(dictionnaire, 15)
-    print(caractere3)
+    # caractere1 = check_dico(dictionnaire, val1)
+    # print("caractère correspondant à " + str(val1) + " --> " + caractere1)
+    # caractere2 = check_dico(dictionnaire, val2)
+    # print("caractère correspondant à " + str(val2) + " --> " + caractere2)
+    # caractere3 = check_dico(dictionnaire, val3)
+    # print("caractère correspondant à " + str(val3) + " --> " + caractere3)
+
+    print("Entrer valeur numérique: ")
+    val = input()   # entrée pour tests
+    val = int(val)  # conversion en int
+    caractere = check_dico(dictionnaire, val)   # check dans dictionnaire
+    print(caractere)    # affichage caractère si existant
 
 
 if __name__ == '__main__':
